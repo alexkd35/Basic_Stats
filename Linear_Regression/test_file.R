d03640ec4189d216e6d4db2564c19bf7b4c200db
alexkd35 <140621966+alexkd35@users.noreply.github.com>
2024-10-11 16:45:58 +0200

    Add files via upload

---
+++ Linear_Regression/test_file.R
@@ -0,0 +1,11 @@
+#Define Working Directory
+setwd("C:/Users/User/OneDrive/DS/R studio/Projects/Linear Regression")
+getwd()
+
+#Load Packages
+install.packages("tidyverse")
+library(tidyverse)
+
+#Prüfen ob NA Werte vorhanden sind -> Keine
+sum(is.na(data))
+
